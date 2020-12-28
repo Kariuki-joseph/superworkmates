51543e091f4cdee007fc4013128ffc614bf6a530
Joel <njoroge.joel2@gmail.com>
2020-12-28 20:06:28 +0300

    Fixed: Added profilepictures folder

--- connections/dbconnect.php
+++ connections/dbconnect.php
@@ -1,9 +1,13 @@
 <?php
 //params to connect to a database
-    $dbHost = "localhost";
-    $dbUser = "root";
-    $dbPass = "";
-    $dbName = "mainusers";
+    // $dbHost = "localhost";
+    // $dbUser = "root";
+    // $dbPass = "";
+    // $dbName = "mainusers";   
+    $dbHost = "host31";
+    $dbUser = "superwor_admin";
+    $dbPass = "5 Potatoex1";
+    $dbName = "superwor_mainusers";
 //connection to database
 $connect = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
 

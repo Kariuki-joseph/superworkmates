663aef8ec57d9ae439c1dca5f45f31181ddd356f
Joel <njoroge.joel2@gmail.com>
2021-01-02 15:07:54 +0300

    Updated php mailer

--- ProcessResetPassword.php
+++ ProcessResetPassword.php
@@ -41,16 +41,60 @@ $to = $userEmail;
 $subject = 'Password Reset for Superworkmates.com';
 $message = '<p> We received a request to change your password. Here is your password reset link: </p>';
 $message .= '<a href="'.$url.'">'.$url.'</a>';
-$button = '<p><a href="'.$url.'"><button> Reset Password </button></a></p>';
-
+$message .= '<p><a href="'.$url.'"><button> Reset Password </button></a></p>';
 $header = "From: <support@superworkmates.com>\r\n";
 $header .= "Reply-To: support@superworkmates.com \r\n";
 $header .="Content-type: text/html\r\n";
 
-mail($to,$subject, $message, $button, $header);
+// mail($to,$subject, $message, $button, $header);
 
-header("Location:resetpassword.php?reset=success");
+// header("Location:resetpassword.php?reset=success");
 
+
+require "phpMailer/PHPMailer/PHPMailerAutoload.php";
+
+function smtpmailer($to, $from, $from_name, $subject, $body)
+    {
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true; 
+ 
+        $mail->SMTPSecure = 'ssl'; 
+        $mail->Host = 'superworkmates.com';
+        $mail->Port = 465;  
+        $mail->Username = 'support@superworkmates.com';
+        $mail->Password = '5 Potatoex1';   
+   
+   //   $path = 'reseller.pdf';
+   //   $mail->AddAttachment($path);
+   
+        $mail->IsHTML(true);
+        $mail->From="support@superworkmates.com";
+        $mail->FromName=$from_name;
+        $mail->Sender=$from;
+        $mail->AddReplyTo($from, $from_name);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AddAddress($to);
+        if(!$mail->Send())
+        {
+            header("Location:resetpassword.php?reset=fail");
+
+        }
+        else 
+        {
+            header("Location:resetpassword.php?reset=success");
+
+        }
+    }
+    
+    $to   = $to;
+    $from = 'support@superworkmates.com';
+    $name = 'Superworkmates.com';
+    $subj = $subject;
+    $msg = $message;
+    
+    $error=smtpmailer($to,$from, $name ,$subj, $msg);
 }
 /*else if not set - button*/
 else {
